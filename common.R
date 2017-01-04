da20fced48c29bda5f8e3d14015fc6b3bd16d752
Tadas Danielius <tadas.danielius@gmail.com>
2017-01-04 12:56:36 +0200

    log-likelihood fixed

--- common.R
+++ common.R
@@ -1,4 +1,4 @@
-impute = function(idx, V,M,D,s) {
+impute = function(idx, V, M, D, s) {
   I = diag(dim(D)[2])
   y = t(t(D[idx, ]))
   S = I * s[idx,]
@@ -6,7 +6,7 @@ impute = function(idx, V,M,D,s) {
   return(y + S_star %*% (M + V %*% S %*% ginv(S %*% V %*% S) %*% (y - S %*% M)))
 }
 
-calc_W = function(idx, V, s){
+calc_W = function(idx, V, s) {
   I = diag(dim(V)[1])
   S = I * s[idx,]
   S_star = I - S
@@ -14,12 +14,16 @@ calc_W = function(idx, V, s){
 }
 
 calc_log_likelihood = function(D, V, M) {
-  sm = 0
-  nrows = dim(D)[1]
-  for (y in as.data.frame(t(D))) {
-    sm = sm+ t(y-M) %*% ginv(V) %*% (y-M)
-  }
-  return(-nrows/2 * log(det(V)) - 1/2 * sm)
+  tryCatch({
+    sm = 0
+    nrows = dim(D)[1]
+    for (y in as.data.frame(t(D))) {
+      sm = sm + t(y - M) %*% ginv(V) %*% (y - M)
+    }
+    return(-nrows / 2 * log(abs(det(V))) - 1 / 2 * sm)
+  }, error = function(err) {
+    message('error happened')
+  })
 }
 
 
@@ -32,45 +36,82 @@ calc_err_cov = function(nrows, V, s, w.sum) {
   return(w.sum)
 }
 
-run_iterations = function(m, max=100, epsilon=0.00001) {
+run_iterations = function(m,
+                          max = 1000,
+                          epsilon = 0.00001,
+                          correlated = TRUE,
+                          ml.cap = 2) {
   D = m$D
   D_prev = D
   
   
   M = m$M
+  
   V = m$V
+  if (!correlated) {
+    V = m$V_diag
+  }
   s = m$s
   
   nrows = dim(D)[1]
   ncols = dim(D)[2]
   ets_err = matrix(0, ncol=1, nrow=max)
-  for (iteration in 1:max) {
-    w.sum = matrix(0, ncol = ncols, nrow = nrows)
-    D = t(sapply(1:nrows, 
-                 impute, 
-                 V=V, 
-                 M=M, 
-                 D=m$D, 
-                 s=s))
-    eps = sum(abs(D_prev-D))
-    ets_err[iteration,] = eps
-    D_prev = D
-    if (eps < epsilon) {
-      message('epsilon ',eps, ' < ', epsilon, ' Terminating.' )
-      break;
-    }
-    w.sum = calc_err_cov(nrows, V, s, w.sum)
-    M = as.matrix(colMeans(D, na.rm=T))
-    V <- cov(D, use = "pairwise.complete.obs") + w.sum
-    ml = calc_log_likelihood(D, V, M)
-    message('iteration ', iteration, ' ML: ', ml, ' Epsilon: ', eps)
+  ml.prev = calc_log_likelihood(D, V, M)
+  if (is.nan(ml.prev)) {
+    warning('ml.prev is NaN')
   }
   
-  ret = list()
-  ret$D = D
-  ret$M = M
-  ret$V = V
-  ret$err = ets_err
-  return (ret)
-}
+  ml = NULL
+  iteration = 1
+  ml.abs = epsilon + 1
+  
+  ml.all = vector()
+  
+  while (ml.abs > epsilon) {
+    not_acceptable = T
+    ml.prev = ifelse(iteration > 1, ml, ml.prev)
+    
+    if (iteration > max) {
+      #message('terminated. Max iteration reached')
+      break
+      
+    }
+    
+    w.sum = matrix(0, ncol = ncols, nrow = nrows)
+    D = t(sapply(
+      1:nrows,
+      impute,
+      V = V,
+      M = M,
+      D = m$D,
+      s = s
+    ))
+    ets_err[iteration,] = eps
+    D_prev = D
+    w.sum = calc_err_cov(nrows, V, s, w.sum)
+    M = as.matrix(colMeans(D, na.rm = T))
+    V <- cov(D, use = "pairwise.complete.obs") + w.sum
+    
+    ml = calc_log_likelihood(D, V, M)
+    ml.abs = abs(ml - ml.prev) / abs(ml.prev)
+    ml.all[iteration] = ml.abs
+    if (is.nan(ml.abs)) {
+      message('ML abs is NaN. ml=', ml, ', ml.prev=', ml.prev)
+      #ml.abs = 0
+    }
+    if (ml.abs > ml.cap && iteration > 1) {
+      ml.all[iteration] = ml.all[iteration-1]
+                                 
+    }
 
+    iteration = iteration + 1
+}
+  
+ret = list()
+ret$D = D
+ret$M = M
+ret$V = V
+  ret$err = ets_err
+
+return (ret)
+}
--- lab2.R
+++ lab2.R
@@ -12,17 +12,52 @@ imputed = t(sapply(1:dim(matrices$D)[1],
                     M=matrices$M, 
                     D=matrices$D, 
                     s=matrices$s))
+
+
 print (imputed)
 
 
 # B Dalis
 m = load_data()
-results = run_iterations(m, max=1000)
-
+results = run_iterations(m, max=1000, epsilon = 0.001, correlated=FALSE, ml.cap=10)
+plot(results$ml.all, type='l')
 print(results$D)
 
 
 # Monte-carlo
-results = simulate.run(100, 0.9, F)
+n = 100
 summary(results$diff)
+summary(results$errors)
+summary(results$mape)
+plot(results$mape,type='l')
+plot(results$errors,type='l')
 
+res_corr = simulate.run(n, 0.001, T, correlated = T)
+summary(res_corr$errors)
+summary(res_corr$mape)
+plot(res_corr$mape, type='l')
+plot(res_corr$errors, type='l')
+
+par(mfrow=c(1,2))
+
+g_range <- range(0, results$errors, res_corr$errors)
+plot(results$errors,type='l', col='red', ylim=g_range, ann=F)
+lines(res_corr$errors, col='blue')
+lines(rep(mean(res_corr$errors), length(res_corr$errors)), type="l", pch=22, lty=4, col="blue")
+lines(rep(mean(results$errors), length(results$errors)), type="l", pch=22, lty=4, col="red")
+legend(1, 1, c("Uncorrelated","Correlated"), cex=0.8, col=c("red","blue"), pch=22)
+title(xlab='Iteracijos')
+title(ylab='Vidutinis skirtumas tarp realių ir užpildytų')
+
+
+g_range <- range(0, results$mape, res_corr$mape)
+plot(results$mape,type='l', col='red', ann=F)
+lines(res_corr$mape, col='blue')
+lines(rep(mean(res_corr$mape), length(res_corr$mape)), type="l", pch=22, lty=4, col="blue")
+lines(rep(mean(results$mape), length(results$mape)), type="l", pch=22, lty=4, col="red")
+legend(1, 0.4, c("Uncorrelated","Correlated"), cex=0.7, col=c("red","blue"), pch=22)
+title(xlab='Iteracijos')
+title(ylab='Mean Absolute % error (MAPE)')
+
+
+plot(res_corr$ml, type='l')
